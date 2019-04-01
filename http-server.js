49dfc97a065b2b011fcbe392000d90806d5c88be
mihaitmf <mihai.tmf@gmail.com>
2019-04-02 01:10:35 +0300

    Update the http server to return a json response

--- http-server.js
+++ http-server.js
@@ -1,16 +1,27 @@
 const http = require('http');
+const url = require('url');
 
 const hostname = '192.168.29.11';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
 
-    let responseBody = 'Hello World\n';
-    responseBody += 'Request method was: ' + req.method + '\n';
-    responseBody += 'Request body was: ';
-    res.write(responseBody);
+    if (req.method === 'GET') {
+        res.setHeader('Content-Type', 'text/plain');
+
+        const urlParts = url.parse(req.url, true);
+
+        let responseBody = 'Hello World\n';
+        responseBody += 'Request method was: ' + req.method + '\n';
+        responseBody += 'Query string params were: ' + JSON.stringify(urlParts.query);
+
+        res.end(responseBody + '\n');
+
+        return;
+    }
+
+    res.setHeader('Content-Type', 'application/json');
 
     req.setEncoding('utf8');
     const requestChunks = [];
@@ -18,10 +29,18 @@ const server = http.createServer((req, res) => {
         console.log('A chunk of data has arrived: ', chunk);
         requestChunks.push(chunk);
     });
+
     req.on('end', () => {
-        const requestBody = requestChunks.join();
-        console.log('Request has ended. Full request body: ', requestBody);
-        res.write(requestBody);
+        const fullRequestBody = requestChunks.join();
+        console.log('Request has ended. Full request body: ', fullRequestBody);
+
+        const responseBody = {
+            message: 'Hello World! We received your request!',
+            requestMethodWas: req.method,
+            requestBodyWas: fullRequestBody
+        };
+
+        res.write(JSON.stringify(responseBody));
         res.end('\n');
     });
 });
