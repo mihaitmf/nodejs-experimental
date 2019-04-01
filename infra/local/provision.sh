a33f8359c14575978fc3b83854cd1aecd7eb77b7
mihaitmf <mihai.tmf@gmail.com>
2019-04-01 21:43:33 +0300

    Install nodejs and npm

--- infra/local/provision.sh
+++ infra/local/provision.sh
@@ -4,3 +4,8 @@ WORK_DIR=/var/node-simple-rest-api
 
 # set working directory after vagrant ssh
 echo "cd $WORK_DIR" >> /home/vagrant/.bashrc
+
+curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
+bash nodesource_setup.sh
+rm -f nodesource_setup.sh
+apt install -y nodejs
