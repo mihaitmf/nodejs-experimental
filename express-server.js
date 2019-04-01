f1cc5d3ca087e757855a056cd885c7409af0e134
mihaitmf <mihai.tmf@gmail.com>
2019-04-02 01:50:16 +0300

    Update express server to parse the POST request and return the params in json

--- express-server.js
+++ express-server.js
@@ -8,12 +8,26 @@ const HOST = '192.168.29.11';
 
 // App
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     let responseBody = 'Hello World\n';
     responseBody += 'Request method was: ' + req.method + '\n';
     responseBody += 'Query string params were: ' + JSON.stringify(req.query);
 
     res.status(200).set('Content-Type', 'text/plain').send(responseBody + '\n');
+});
+
+app.post('/', (req, res) => {
+    console.log(`Request body is: ${JSON.stringify(req.body)}`);
+
+    const responseBody = {
+        message: 'Hello World! We received your request!',
+        requestMethodWas: req.method,
+        requestBodyWas: req.body
+    };
+
+    res.send(responseBody); // sets automatically content-type to application/json when an object is received
 });
 
 app.listen(PORT, HOST);
