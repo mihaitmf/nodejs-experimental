790286870789769bef1e06db6d0dfa19befdcfbf
mihaitmf <mihai.tmf@gmail.com>
2019-04-01 23:59:12 +0300

    Fix indentation

--- http-server.js
+++ http-server.js
@@ -5,8 +5,8 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-res.setHeader('Content-Type', 'text/plain');
-res.end('Hello World\n');
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
 });
 
 server.listen(port, hostname, () => {
