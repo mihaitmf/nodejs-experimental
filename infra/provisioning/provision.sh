3ffdc7d58ccf5a5ebd829eaaf5d06055569afead
mihaitmf <mihai.tmf@gmail.com>
2019-04-02 20:16:27 +0300

    Add comment in provisioner

--- infra/provisioning/provision.sh
+++ infra/provisioning/provision.sh
@@ -5,6 +5,7 @@ WORK_DIR=/var/nodejs-experimental
 # set working directory after vagrant ssh
 echo "cd $WORK_DIR" >> /home/vagrant/.bashrc
 
+# install nodejs, it includes also npm (optional step because we run node from docker, but just to experiment)
 curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
 bash nodesource_setup.sh
 rm -f nodesource_setup.sh
