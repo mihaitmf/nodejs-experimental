196579df3126db33e692e8cd6fbed8dd9c55ce62
mihaitmf <mihai.tmf@gmail.com>
2019-04-02 16:13:48 +0300

    Add shell scripts for docker build and run commands

---
+++ infra/local/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(realpath $(dirname "$0"))
+
+docker build \
+    --tag mihaitmf/node-express-service:latest \
+    --file ${SCRIPT_DIR}/../../Dockerfile \
+    ${SCRIPT_DIR}/../../
---
+++ infra/local/run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+IMAGE_NAME="mihaitmf/node-express-service:latest"
+CONTAINER_NAME="node-express-service"
+HOST_PORT=8080
+
+# stop and remove if exists
+(docker stop ${CONTAINER_NAME} && docker rm ${CONTAINER_NAME}) > /dev/null 2>&1
+
+# start container from scratch
+docker run -d \
+    --name ${CONTAINER_NAME} \
+    -p ${HOST_PORT}:8080 \
+    ${IMAGE_NAME}
