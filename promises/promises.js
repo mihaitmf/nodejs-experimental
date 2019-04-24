48262c6cac64004f9e777aac69ba2ca9ff05590f
mihaitmf <mihai.tmf@gmail.com>
2019-04-24 14:50:20 +0300

    Chain "then()" calls

--- promises/promises.js
+++ promises/promises.js
@@ -12,6 +12,7 @@ function initialize() {
     // Return new promise
     return new Promise(function(resolve, reject) {
         // Do async job
+        console.log("Make request");
         request.get(options, function(err, resp, body) {
             if (err) {
                 reject(err);
@@ -20,18 +21,27 @@ function initialize() {
             }
         })
     })
-
 }
 
 function main() {
+    console.log("Create promise");
     const initializePromise = initialize();
+
     initializePromise.then(function(result) {
         userDetails = result;
         console.log("Initialized user details");
+
         // Use user details from here
-        console.log(userDetails)
+        console.log(userDetails);
+
+        return userDetails;
+
     }, function(err) {
-        console.log(err);
+        console.log(`Error: ${err}`);
+
+    }).then(function(result) {
+        const countRepos = result.public_gists + result.public_repos;
+        console.log(`Count public repos and gists: ${countRepos}`);
     });
 }
 
