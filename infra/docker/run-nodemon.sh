c3bb2786dc8b65f086df1fd7230ac0916bf67c7c
mihaitmf <mihai.tmf@gmail.com>
2020-02-10 18:20:36 +0200

    Use explicit arguments for the docker run commands

--- infra/docker/run-nodemon.sh
+++ infra/docker/run-nodemon.sh
@@ -9,7 +9,7 @@ CONTAINER_NAME="${2:-"node-app"}" # the second command line argument if given, o
 HOST_PORT=${3:-8080} # the third command line argument if given, or default value
 CONTAINER_PORT=${HOST_PORT}
 
-# needed to sync working directory
+# these are used to sync the working directory between host and container
 HOST_WORK_DIR=$(realpath $(dirname $(readlink -f "$0"))/../..)
 CONTAINER_WORK_DIR=/usr/src/app
 
@@ -17,9 +17,9 @@ CONTAINER_WORK_DIR=/usr/src/app
 (docker stop ${CONTAINER_NAME} && docker rm ${CONTAINER_NAME}) > /dev/null 2>&1
 
 # start container from scratch
-docker run -d \
+docker run --detach \
     --name ${CONTAINER_NAME} \
-    -p ${HOST_PORT}:${CONTAINER_PORT} \
+    --publish ${HOST_PORT}:${CONTAINER_PORT} \
     --volume ${HOST_WORK_DIR}:${CONTAINER_WORK_DIR} \
     --workdir ${CONTAINER_WORK_DIR} \
     ${IMAGE_NAME} ${EXEC_FILE}
